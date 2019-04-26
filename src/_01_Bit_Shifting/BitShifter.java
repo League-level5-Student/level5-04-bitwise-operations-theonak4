18767dfb32f97fe8a3916ffb9c50f5aa85f8f8d7
theonak4 <38365756+theonak4@users.noreply.github.com>
2019-04-25 22:27:59 -0700

    done

--- src/_01_Bit_Shifting/BitShifter.java
+++ src/_01_Bit_Shifting/BitShifter.java
@@ -4,14 +4,15 @@ public class BitShifter {
 	public static void main(String[] args) {
 		// 1. Jot down the value of num in binary.
 		int num = 4;
-		int numShifted = num << 0;
-		
+		int numShifted = 37 << 1;
 		// 2. Print the value of numShifted, and convert that value to binary.
-		
-		// 3. Compare the two binary values. Can you figure out what the << operator is for?
-		
+		System.out.println(numShifted);
+		// 3. Compare the two binary values. Can you figure out what the << operator is
+		// for?
+		// shifts
 		// 4. Try shifting num 3 places.
-		
-		// FYI: Binary values can be shifted to the right as well using the >> operator.	
+		// 10 Wins on First 4 Quizzes
+		// FYI: Binary values can be shifted to the right as well using the >> operator.
+		//
 	}
 }
